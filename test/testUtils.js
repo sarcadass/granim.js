a875a4295a796f9467694cddfe2242fcd82e1e61
Benjamin Blonde <sarcadass@gmail.com>
2017-11-06 14:37:46 +0100

    Add Image and Blending Modes tests

--- test/testUtils.js
+++ test/testUtils.js
@@ -9,11 +9,22 @@ function setCanvas(canvasId) {
 
 	canvas = document.createElement('canvas');
 	canvas.setAttribute('id', canvasId || 'granim-canvas');
-	canvas.setAttribute('style', 'position:absolute; width: 100%; height: 150px; left: 0; right: 0;');
+	canvas.setAttribute('style', 'position:absolute; width: 100%; height: 300px; left: 0; right: 0;');
 	document.body.appendChild(canvas);
+	return canvas;
 }
 
 function unsetCanvas(canvasId) {
 	var canvas = document.querySelector('#' + (canvasId || 'granim-canvas'));
 	if (canvas) canvas.remove();
+}
+
+function compareImages(img1,img2){
+	if(img1.data.length != img2.data.length)
+		return false;
+	for(var i = 0; i < img1.data.length; ++i){
+		if(img1.data[i] != img2.data[i])
+			return false;
+	}
+	return true;
 }
