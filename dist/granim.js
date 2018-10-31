bb24abbfe515aab8d24929283769b7dae276067b
JB <aniel.jeanbaptiste@gmail.com>
2018-10-31 15:16:07 +0100

    Implementation of gradient creation with % and px

--- dist/granim.js
+++ dist/granim.js
@@ -766,7 +766,7 @@ module.exports = function() {
 },{}],24:[function(require,module,exports){
 'use strict';
 
-module.exports = function () {
+module.exports = function() {
 	var ctx = this.context;
 
 	switch (this.direction) {
@@ -790,10 +790,21 @@ module.exports = function () {
 			return ctx.createRadialGradient(this.x1 / 2, this.y1 / 2, this.x1 / 2, this.x1 / 2, this.y1 / 2, 0);
 			break;
 		case 'custom':
-			return ctx.createLinearGradient(this.customDirection.x0, this.customDirection.y0, this.customDirection.x1, this.customDirection.y1);
+			return ctx.createLinearGradient(
+				getCustomCoordinateInPixels(this.customDirection.x0, this.x1),
+				getCustomCoordinateInPixels(this.customDirection.y0, this.y1),
+				getCustomCoordinateInPixels(this.customDirection.x1, this.x1),
+				getCustomCoordinateInPixels(this.customDirection.y1, this.y1)
+			);
 			break;
 
 	}
+};
+
+function getCustomCoordinateInPixels(coordinate, size) {
+	return coordinate.indexOf('%') > -1
+		? size / 100 * parseInt(coordinate.split('%')[0], 10)
+		: parseInt(coordinate.split('px')[0], 10);
 };
 
 },{}],25:[function(require,module,exports){
